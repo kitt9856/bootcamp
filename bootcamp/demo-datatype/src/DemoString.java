d1597059b68c9ce3156da38f161b46a89735c70e
peterk <kitt9856@gmail.com>
2024-10-05 17:01:29 +0800

    update

---
+++ bootcamp/demo-datatype/src/DemoString.java
@@ -0,0 +1,24 @@
+public class DemoString { //class name = file anme
+    public static void main (String[] args) {
+        //integer
+
+
+        // ...com is a string value
+        String emailAddress = "ABC@gmail.com";
+        System.out.println(emailAddress);
+
+
+
+        //as 2 is an int value, we cannot assign int value to String variable.
+        //String x = 2; Not ok!!!!!
+
+
+
+        //string operation: +
+        string x1 = "hello";
+        string x2 = "world";
+        string x3 = x1 + x2;
+        System.out.println("x3=" + x3, "x2=" + x2);
+
+    }
+}
